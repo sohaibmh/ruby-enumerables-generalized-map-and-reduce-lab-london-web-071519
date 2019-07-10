e59f4e66a030706c721d88898279ba10b58023b3
Sohaib Mubashir <sohaibmh@yahoo.com>
2019-07-10 21:52:13 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,3 +1,29 @@
 def map(array)
   array.map {|x| x * -1}
 end# Your Code Here
+
+def map(array)
+  array.map { |x| x }
+end
+
+def map(array)
+  array.map {|x| x * 2}
+end
+
+def map(array)
+  array.map {|x| x * x}
+end
+
+
+def reduce(array, starting_point = 0)
+array.reduce(starting_point) {|sum, n| sum + n}
+end
+
+def reduce(array)
+  array.reduce{|e, f| e && f}
+end
+
+
+def reduce(array)
+  array.reduce{|e, f| e or !!f}
+end
